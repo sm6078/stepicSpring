42cd2eef1c9572da9a4f3613f58bc5baa517cdfd
Sergei Poddubniak <forvvard09@gmail.com>
2023-06-05 18:58:47 +0300

    Spring init, destroy methods.

--- src/spring_introduction/Dog.java
+++ src/spring_introduction/Dog.java
@@ -4,16 +4,16 @@ public class Dog implements Pet {
 
     private String name;
 
+    public void init() {
+        System.out.println("Class dog: init method");
+    }
+
     public Dog() {
         System.out.println("Dog bean is created");
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
+    public void destroy() {
+        System.out.println("Class dog: destroy method");
     }
 
     @Override
--- src/spring_introduction/Test4.java
+++ src/spring_introduction/Test4.java
@@ -8,14 +8,14 @@ public class Test4 {
                 new ClassPathXmlApplicationContext("applicationContext2.xml");
 
         Dog myDog = context.getBean("myPet", Dog.class);
-        myDog.setName("Belka");
+//        myDog.setName("Belka");
         Dog yourDog = context.getBean("myPet", Dog.class);
-        yourDog.setName("Strelka");
-
-        context.close();
-
-        System.out.println(myDog.getName());
-        System.out.println(yourDog.getName());
+//        yourDog.setName("Strelka");
+//
+//        context.close();
+//
+//        System.out.println(myDog.getName());
+//        System.out.println(yourDog.getName());
 
         System.out.println("Переменные ссылаются на один и тот же объект ? " + (myDog == yourDog));
         System.out.println(myDog);
